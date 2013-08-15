9199839ac8c767b877a810c5fe984394e7949513
Alexander Dinauer <alexander@dinauer.at>
2013-08-15 19:43:01 +0200

    always include modules matching the dependencyRegex, even if no matching dependencies have been found inside them

--- buildSrc/src/main/groovy/DependencyToDotLanguageTranslator.groovy
+++ buildSrc/src/main/groovy/DependencyToDotLanguageTranslator.groovy
@@ -1,12 +1,16 @@
 class DependencyToDotLanguageTranslator {
 
-	List<String> translateDependenciesToDotLanguage(Map<String, Set<String>> dependenciesGroupedByModule) {
+	List<String> translateDependenciesToDotLanguage(Map<String, Set<String>> dependenciesGroupedByModule, String regex) {
 		List<String> graphEntries = []
+
 		dependenciesGroupedByModule.each { module, dependencies ->
-			graphEntries << "\"$module\";"
 			dependencies.each { dependency ->
 				graphEntries << "\"$module\" -> \"$dependency\";"
 			}
+		}
+
+		dependenciesGroupedByModule.findAll{ it.key ==~ regex }.each{ module, dependencies ->
+			graphEntries << "\"$module\";"
 		}
 
 		return graphEntries
