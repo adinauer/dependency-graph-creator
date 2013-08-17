d813792922f2d5441706c89ef6921bf4af850b50
Alexander Dinauer <alexander@dinauer.at>
2013-08-17 17:25:25 +0200

    add semicolon to generated dot

--- buildSrc/src/main/groovy/GraphvizDotfileCreator.groovy
+++ buildSrc/src/main/groovy/GraphvizDotfileCreator.groovy
@@ -2,7 +2,7 @@ class GraphvizDotfileCreator {
 	void createDirectedGraphFile(File targetFile, List<String> graphEntries) {
 		targetFile.withWriter { out ->
 			out.println 'digraph dependencies {'
-			out.println 'rankdir=LR'
+			out.println 'rankdir=LR;'
 			
 			graphEntries.each {
 				out.println it
