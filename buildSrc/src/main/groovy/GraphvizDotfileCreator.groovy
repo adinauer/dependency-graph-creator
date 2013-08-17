249069c5747ac18c5f40576ccf21fdc586fcf37c
Alexander Dinauer <alexander@dinauer.at>
2013-08-17 18:18:31 +0200

    use thicker lines for interesting edges in html report

--- buildSrc/src/main/groovy/GraphvizDotfileCreator.groovy
+++ buildSrc/src/main/groovy/GraphvizDotfileCreator.groovy
@@ -3,6 +3,7 @@ class GraphvizDotfileCreator {
 		targetFile.withWriter { out ->
 			out.println 'digraph dependencies {'
 			out.println 'rankdir=LR;'
+			out.println 'edge [penwidth=2];'
 			
 			graphEntries.each {
 				out.println it
