ae91cfbb93eef6998d3ebfb1be8700fe3899b791
Alexander Dinauer <alexander@dinauer.at>
2013-08-18 01:17:26 +0200

    refactor dependency to dot language translation

--- buildSrc/src/main/groovy/DependencyToDotLanguageTranslator.groovy
+++ buildSrc/src/main/groovy/DependencyToDotLanguageTranslator.groovy
@@ -3,18 +3,49 @@ class DependencyToDotLanguageTranslator {
 	List<String> translateDependenciesToDotLanguage(Map<String, Set<String>> dependenciesGroupedByModule, String regex) {
 		List<String> graphEntries = []
 
+		def matchingDependenciesGroupedByModule = dependenciesGroupedByModule.findAll{ it.key ==~ regex }
+
+		graphEntries.addAll graphEntriesForDependencies(dependenciesGroupedByModule)
+		graphEntries.addAll graphEntriesForMatchedNodes(matchingDependenciesGroupedByModule)
+		graphEntries.addAll graphEntriesForNonMatchedNodes(dependenciesGroupedByModule, matchingDependenciesGroupedByModule)
+
+		return graphEntries
+	}
+
+	Set<String> collectAllDependencies(Map<String, Set<String>> dependenciesGroupedByModule) {
+		def allDependencies = [] as Set
+
+		dependenciesGroupedByModule.each { module, dependencies ->
+			allDependencies.addAll dependencies
+		}
+
+		return allDependencies
+	}
+
+	List<String> graphEntriesForDependencies(Map<String, Set<String>> dependenciesGroupedByModule) {
+		List<String> graphEntries = []
+
 		dependenciesGroupedByModule.each { module, dependencies ->
 			dependencies.each { dependency ->
 				graphEntries << "\"$module\" -> \"$dependency\";"
 			}
 		}
 
-		def matchingDependenciesGroupedByModule = dependenciesGroupedByModule.findAll{ it.key ==~ regex }
+		return graphEntries
+	}
+
+	List<String> graphEntriesForMatchedNodes(Map<String, Set<String>> matchingDependenciesGroupedByModule) {
+		List<String> graphEntries = []
 
 		matchingDependenciesGroupedByModule.each{ module, dependencies ->
 			graphEntries << "\"$module\" [fontcolor=blue, color=blue, label=<<TABLE border=\"0\"><TR><TD title=\"$module\" href=\"javascript:redrawGraph('$module');\">$module</TD></TR></TABLE>>];"
 		}
 
+		return graphEntries
+	}
+
+	List<String> graphEntriesForNonMatchedNodes(Map<String, Set<String>> dependenciesGroupedByModule, Map<String, Set<String>> matchingDependenciesGroupedByModule) {
+		List<String> graphEntries = []
 
 		def allDependencies = collectAllDependencies(dependenciesGroupedByModule)
 
@@ -25,15 +56,5 @@ class DependencyToDotLanguageTranslator {
 		}
 
 		return graphEntries
-	}
-
-	Set<String> collectAllDependencies(Map<String, Set<String>> dependenciesGroupedByModule) {
-		def allDependencies = [] as Set
-		
-		dependenciesGroupedByModule.each { module, dependencies ->
-			allDependencies.addAll dependencies
-		}
-
-		return allDependencies
 	}
 }
