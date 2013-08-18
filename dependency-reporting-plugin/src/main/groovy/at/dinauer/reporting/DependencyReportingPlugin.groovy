c7ef2122b7385ee26abb28c2830ac70c976d5bc8
Alexander Dinauer <alexander@dinauer.at>
2013-08-18 20:34:59 +0200

    move openDependencyReport task declaration into plugin

--- dependency-reporting-plugin/src/main/groovy/at/dinauer/reporting/DependencyReportingPlugin.groovy
+++ dependency-reporting-plugin/src/main/groovy/at/dinauer/reporting/DependencyReportingPlugin.groovy
@@ -2,10 +2,18 @@ package at.dinauer.reporting
 
 import org.gradle.api.*
 
+import java.awt.Desktop
+
 class DependencyReportingPlugin implements Plugin<Project> {
     void apply(Project project) {
+        def dependencyReportDir = "${project.buildDir}/report/dependencies"
+
         project.task('helloFromPlugin') << {
             println 'hello from the plugin!'
+        }
+
+        project.task('openDependencyReport') << {
+            Desktop.getDesktop().open(new File("$dependencyReportDir/dependencies.html"))
         }
     }
 }
